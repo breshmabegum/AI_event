3437dc8d81a70d43ec9511a4c4aa00844a0f6b8b
breshmabegum <breshmabegum87@gmail.com>
2026-08-03 11:21:10 +0530

    Update guest URL

--- organizer.py
+++ organizer.py
@@ -78,7 +78,7 @@ if st.button("Upload"):
 if st.button("Generate QR Code"):
     os.makedirs("qrcodes", exist_ok=True)
 
-    url = f"https://aievent-h73cwd74dkwumvhmlemrhu.streamlit.app/?event={event_name}"
+    url = f"https://aievent-mskmt4gfzymwacaryet45u.streamlit.app/?event={event_name}"
     st.write(url)
 
     img = qrcode.make(url)
